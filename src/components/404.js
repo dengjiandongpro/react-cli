94d5a4a4d5a5cafa7558fcab5b1c784aa0ee5118
DengJiandong <434517322@qq.com>
2020-06-19 10:13:58 +0800

    test vscode git

--- src/components/404.js
+++ src/components/404.js
@@ -5,14 +5,11 @@ import PropTypes from 'prop-types';
 
 class NoaMatch extends Component {
     render() {
-
-      return (
-        <div className="out">
-           
-        </div>
-      )
+        return ( 
+            <div className="out"></div>
+        )
     }
-  }
-  //counter中函数内的属性设置
- 
+}
+//counter中函数内的属性设置
+
 export default NoaMatch
