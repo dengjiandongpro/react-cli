465936f4f98d62f54417d4ad1cf2e0a78a653cea
DengJiandong <434517322@qq.com>
2021-01-05 11:11:58 +0800

    test

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import Home from './components/home'
 import NoaMatch from './components/404'
 
 class App extends React.Component {
-    render(){ // Every react component has a render method. 
-      return( // Every render method returns jsx. Jsx looks like HTML, but it's actually javascript and functions a lot like xml, with self closing tags requiring the `/` within the tag in order to work propperly
+    render(){ 
+      return( 
         <div>
             <ul>
                 <li>
